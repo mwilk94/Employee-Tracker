26be5546bc3007f6c5648371cf9023c3aad09a38
Merkisha Wilkinson <merkisha94@gmail.com>
2022-10-03 19:43:48 -0400

    updated seed data with values

--- db/seeds.sql
+++ db/seeds.sql
@@ -7,3 +7,28 @@ VALUES
     ('Sales'),
     ('Finance'),
     ('Legal');
+
+INSERT INTO role
+    (title, salary, department_id)
+VALUES
+    ('Sales Lead', 100000, 1),
+    ('Salesperson', 80000, 1),
+    ('Lead Engineer', 150000, 2),
+    ('Software Engineer', 120000, 2),
+    ('Account Manager', 160000, 3),
+    ('Accountant', 125000, 3),
+    ('Legal Team Lead', 250000, 4),
+    ('Lawyer', 190000, 4);
+
+INSERT INTO employee
+    (first_name, last_name, role_id, manager_id)
+VALUES
+    ('Don', 'Lothario', 3, NULL),
+    ('Bella', 'Goth', 1, 3),
+    ('Judith', 'Ward', 8, Null);
+    ('Bob', 'Pancakes', 2, 1),
+    ('Summer', 'Holiday', 7, NULL),
+    ('Agness', 'Crumplebottom', 4, NULL),
+    ('Vladius', 'Straud', 6, 5),
+    ('Geoffrey', 'Landgraab', 5, 7),
+    
